78fbeef09c4bcfd52cb9da8dfe34bb9ad438eb9f
saraMir26 <mirz0014@algonquinlive.com>
2026-07-13 00:49:45 -0400

    Test GitHub Actions formatting failure

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -13,7 +13,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "main" {
-  name     = var.resource_group_name
-  location = var.location
+  name=var.resource_group_name
+  location=var.location
 }
 
